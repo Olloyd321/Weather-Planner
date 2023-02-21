eb3078fe3e9a5c25d2893847468f64304604915f
Olloyd321 <olloyd321@gmail.com>
2023-02-21 18:52:40 -0500

    added more javascript to display current citys weather data

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,9 @@
 // const APIURL = 'https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={74190a20c1ddbdc4f115b7fc58fd24ac}'
 // const GEOAPIURL = 'http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid={74190a20c1ddbdc4f115b7fc58fd24ac}'
 
-
-
-
 const apiKey = "74190a20c1ddbdc4f115b7fc58fd24ac";
-const APIURL = `https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=${apiKey}`
+const APIURL = `https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=${apiKey}`;
+const APIURLForecast = `https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=${apiKey}`;
 
 const urlApiCall = (location) =>
     `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}&units=imperial`;
@@ -14,13 +12,12 @@ function getWeatherByLocation(location){
     fetch (urlApiCall(location)).then(function (response){
         return response.json();
     }).then(function(data){
-        console.log(data);
-        console.log(data.main.temp);
-        console.log(data.wind);
-        console.log(data.main.humidity);
-        document.getElementById("currentTemperatureResult").innerHTML=(data.main.temp);
-        
-    })
+        document.getElementById("currentResultCity").innerHTML=(data.name);    
+        document.getElementById("currentTemperatureResult").innerHTML=(data.main.temp)+" Degrees";
+        document.getElementById("currentHumidityResult").innerHTML=(data.main.humidity);
+        document.getElementById("currentWeatherResult").innerHTML=(data.weather[0].description);
+        document.getElementById("currentWindResult").innerHTML=(data.wind.speed)+" mph";
+    });
     // const response = fetch(urlApiCall(location));
     // console.log(response);
     // const responseData = response.json();
